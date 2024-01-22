844c769be51affa2a8316aa656bba8a01fefcb6c
Sf-Git-2023 <sf.chang232tpv@gmail.com>
2024-01-22 15:40:47 +0800

    UD 0122-1540

--- pico_w/lesson11_1.py
+++ pico_w/lesson11_1.py
@@ -1,5 +1,11 @@
 from machine import Pin
+import time
 
 red_led = Pin(15,mode=Pin.OUT)
-red_led.value(1)
-red_led.value(0)
+btn = Pin(14,mode=Pin.PULL_DOWN)
+
+while True:
+    if btn.value():
+        red_led.value(1)
+    else:
+        red_led.value(0)

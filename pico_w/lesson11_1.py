6d21a3214f38b1d9d55e9b3d78953774cd892dc2
Sf-Git-2023 <sf.chang232tpv@gmail.com>
2024-01-24 09:25:30 +0800

    UD 0124-24

--- pico_w/lesson11_1.py
+++ pico_w/lesson11_1.py
@@ -6,28 +6,36 @@ import urequests
 red_led = Pin(15,mode=Pin.OUT)
 btn = Pin(14,mode=Pin.PULL_DOWN)
 is_press = False
-connect()
+#connect()
 
 while True:
     if btn.value():
-        is_press = True
-        red_led.value(1)
+        time.sleep_ms(50)
+        if btn.value():
+            is_press = True
+            red_led.value(1)
     else:
-        if is_press == True:
-            print('release')
-            is_press = False
-            url_str = 'https://openapi-scdc.onrender.com/pico_w/2024-01-22 16:02:10?address=chicken_KFC&celsius=15.386'
-            try:
-                response = urequests.get(url_str)            
-            except:
-                print("ap出現問題")            
-                reconnect()
-            else:
-                if response.status_code == 200:            
-                    print("傳送訊息成功")
+        time.sleep_ms(50)
+        if btn.value() == False:
+            if is_press == True:     
+                print('release')
+                is_press = False
+                '''
+                url_str = 'https://自已的網址/pico_w/2024-01-22 16:02:10?address=chicken_KFC&celsius=15.386'
+                try:
+                    response = urequests.get(url_str)
+                    pass
+                except:
+                    print("ap出現問題")            
+                    reconnect()
                 else:
-                    print("傳送失敗(server出現錯誤)")
-                response.close()
+                    if response.status_code == 200:            
+                        print("傳送訊息成功")
+                    else:
+                        print("傳送失敗(server出現錯誤)")
+                    response.close()
+                '''
             
         
-        red_led.value(0)
+            red_led.value(0)
+        

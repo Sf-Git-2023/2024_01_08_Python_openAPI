e9604252e06ea336385f54a4492909e29d8c1254
Sf-Git-2023 <sf.chang232tpv@gmail.com>
2024-01-24 15:40:52 +0800

    UD 0124-1540

--- openAPI/main.py
+++ openAPI/main.py
@@ -11,10 +11,10 @@ def read_root():
     counter = redis_conn.incr('test:increment',1)
     return {"Counter": counter}
 
-@app.get("/counter/{c}")
-def counter(c:int):
-    counter = redis_conn.incr('test:increment',c)
-    return {"Counter": counter}
+#@app.get("/counter/{c}")
+#def counter(c:int):
+#    counter = redis_conn.incr('test:increment',c)
+#    return {"Counter": counter}
 
 
 @app.get("/pico_w/{date}")
